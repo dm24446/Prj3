f8287c5f50f5b00a6497e686940add98cae4c93a
David <dmoreno2@hawk.iit.edu>
2017-11-26 00:33:46 -0600

    added working jscript to Design page

---
+++ site.js
@@ -0,0 +1,12 @@
+$('html').addClass('js').removeClass('no-js');
+
+$('u[href]').on('click', function(event) {
+  window.open($(this).attr('href'), "_self");
+  event.preventDefault();
+});
+$('abbr[title]').one('click', function() {
+  var expansion = $(this).attr('title');
+  var abbr = $(this).text();
+  $(this).text(expansion + " (" + abbr + ")");
+  $(this).addClass('is-expanded');
+});
