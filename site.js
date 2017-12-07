9236d8e9a6ff07c8a71e81228a5eef9315e893fc
David <dmoreno2@hawk.iit.edu>
2017-12-07 03:59:07 -0600

    Fun with JQuries

--- site.js
+++ site.js
@@ -1,12 +1,20 @@
 $('html').addClass('js').removeClass('no-js');
-
+$('h3').hide();
 $('a[href]').on('click', function(event) {
-  window.open($(this).attr('href'), "_self");
+  window.open($(this).attr('href'), '_self');
   event.preventDefault();
 });
+$('button[href]').on('click', function(event) {
+  window.open($(this).attr('href'), '_blank');
+  event.preventDefault();
+});
+$('button[class]').click(function(event){
+  $(this).hide();
+  $('h3').show();
+  });
 $('abbr[title]').one('click', function() {
   var expansion = $(this).attr('title');
   var abbr = $(this).text();
-  $(this).text(expansion + " (" + abbr + ")");
+  $(this).text(expansion + ' (' + abbr + ')');
   $(this).addClass('is-expanded');
 });
